322e01536fa538724c18941804161872eef150ba
m ! <143506390+iwmillyx@users.noreply.github.com>
2023-08-31 16:14:00 -0300

    Add files via upload

---
+++ três números inteiros, retorna o maior.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+    //dicionário de dados
+    int a, b, c;
+    int maior;
+
+    
+    //obtém os três números
+    printf("Digite os tres numeros separados por espaco: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    //encontra o maior número
+    if(a > b && a > c) {
+        maior = a;
+    }
+    else if(b > c){
+        maior = b;
+    }
+    else {
+        maior = c;
+    }
+    //exibe o resultado
+    printf("O maior numero eh: %d \n", maior);
+    return 0;
+    
+} //FIM
