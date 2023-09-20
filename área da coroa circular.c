c7910e95bd1a08b517a22e94d995c10c4e2c8c90
m ! <143506390+iwmillyx@users.noreply.github.com>
2023-09-20 13:51:56 -0300

    Update área da coroa circular.c

--- área da coroa circular.c
+++ área da coroa circular.c
@@ -17,3 +17,29 @@ int main (){
   return 0;
 
 }//FIM
+
+ -OUTRA ALTERNATIVA-
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    float r1, r2, area;
+    
+    printf("Digite um número: \n");
+    scanf("%g", &r1);
+    
+    printf("Digite outro número: \n");
+    scanf("%g", &r2);
+    
+    area = 3.14 * (r1 * r1 - r2 * r2);
+    
+    
+    printf("O valor da área é: %g \n", area);
+    
+    return 0;
+    
+}
+
+
